57a4a32f1a07fa3951fc1ef69eb84b0c7fbd1380
TetianaMaltseva <92920849+TetianaMaltseva@users.noreply.github.com>
2021-10-28 15:21:15 +0300

    js

---
+++ gr2_maltseva_tetiana_dz1/gr2_maltseva_tetiana_dz1/js/script.js
@@ -0,0 +1,25 @@
+let my_surname = 'Maltseva';
+console.log(my_surname);
+const NAME = ' Tetiana';
+console.log(NAME);
+const SEX_OF_A_PERSON = 'woman';
+console.log(SEX_OF_A_PERSON);
+let my_age =  40;
+console.log(my_age);
+let status = 'married';
+const CHILDREN = 'I have a son, his name is Mark';
+console.log(CHILDREN);
+let adress =  'Kramatorsk, Marii Priymachenko';
+console.log(adress);
+const EYE_COLOR = 'brown';
+console.log(EYE_COLOR);
+let my_hair =  'light brown, medium long';
+console.log(my_hair);
+let my_height =  170;
+console.log(my_height);
+let my_weight =  48;
+console.log(my_weight);
+
+
+
+
